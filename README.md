416f617bd9d7b5b7bd1aa4fa57bf58dbc4b7950c
Mike <ballardm123@gmail.com>
2020-04-07 11:50:49 +0100

    Readme updated

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-Testing NPM Publishing
+# React Responsive Bricks
+
+A responsive masonry layout for React
+
+## [Live Demo](https://mickybizzle.github.io/react-responsive-bricks/)
+
+## Installation
+
+```
+#NPM
+npm i --save react-responsive-bricks
+
+#Yarn
+yarn add react-responsive-bricks
+```
+
+## Usage
+
+```JSX
+import React from "react";
+import { Bricks } from "react-responsive-bricks";
+
+//...
+
+let data = [
+  {
+    imSrc: "link to image"
+  },
+  {
+    imSrc: "link to image"
+  },
+  {
+    imSrc: "link to image"
+  },
+]
+
+//...
+
+<Bricks data={data} />
+```
+
+## Props
+
+| Prop            | Type     | Default | Description                                                                                                                                       |
+| --------------- | -------- | ------- | ------------------------------------------------------------------------------------------------------------------------------------------------- |
+| data            | `Array`  |         | Array of objects in the form `{imgSrc: "link to img"}`. _Required_                                                                                |
+| columns         | `Number` | 4       | How many columns to show. Minimum of 1. Has built in logic for responsiveness.                                                                    |
+| gap             | `Number` | 0       | Gap (in pixels) between elements. Minimum of 0                                                                                                    |
+| minWidth        | `Number` | 200     | Minimum width (in pixels) of each element. Determines wrapping of elements based on width of the browser.                                         |
+| backgroundColor | `String` | white   | Background color of the grid. Only seen if `gap` is more than 0. Supports named colors (e.g. white, red, black, etc) or hex values (e.g. #ffffff) |
+
+## Todo
+
+This is my list of features I plan to implement in the near future. If you have any ideas reach out!
+
+- [ ] onClick for each element
+- [ ] Support for cards as well as images
+- [ ] Add more mouseover features
--- src/App.js
+++ src/App.js
@@ -1,12 +1,199 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import "./App.scss";
 import { Bricks } from "./Bricks/index";
 
 function App() {
+  // data,
+  // columns = 4,
+  // gap = 0,
+  // minWidth = 200,
+  // backgroundColor = "white",
+  const [columns, setColumns] = useState(4);
+  const [gap, setGap] = useState(0);
+  const [minWidth, setMinWidth] = useState(200);
+  const [backgroundColor, setBackgroundColor] = useState("white");
+  const [data, setData] = useState([
+    {
+      imgSrc: "https://source.unsplash.com/random/1",
+    },
+    {
+      imgSrc: "https://source.unsplash.com/random/2",
+    },
+    {
+      imgSrc: "https://source.unsplash.com/random/3",
+    },
+    {
+      imgSrc: "https://source.unsplash.com/random/4",
+    },
+    {
+      imgSrc: "https://source.unsplash.com/random/5",
+    },
+    {
+      imgSrc: "https://source.unsplash.com/random/6",
+    },
+    {
+      imgSrc: "https://source.unsplash.com/random/7",
+    },
+    {
+      imgSrc: "https://source.unsplash.com/random/8",
+    },
+    {
+      imgSrc: "https://source.unsplash.com/random/9",
+    },
+    {
+      imgSrc: "https://source.unsplash.com/random/10",
+    },
+    {
+      imgSrc: "https://source.unsplash.com/random/11",
+    },
+    {
+      imgSrc: "https://source.unsplash.com/random/12",
+    },
+    {
+      imgSrc: "https://source.unsplash.com/random/13",
+    },
+    {
+      imgSrc: "https://source.unsplash.com/random/14",
+    },
+    {
+      imgSrc: "https://source.unsplash.com/random/15",
+    },
+    {
+      imgSrc: "https://source.unsplash.com/random/16",
+    },
+    {
+      imgSrc: "https://source.unsplash.com/random/17",
+    },
+    {
+      imgSrc: "https://source.unsplash.com/random/18",
+    },
+    {
+      imgSrc: "https://source.unsplash.com/random/19",
+    },
+    {
+      imgSrc: "https://source.unsplash.com/random/20",
+    },
+    {
+      imgSrc: "https://source.unsplash.com/random/21",
+    },
+    {
+      imgSrc: "https://source.unsplash.com/random/22",
+    },
+    {
+      imgSrc: "https://source.unsplash.com/random/23",
+    },
+    {
+      imgSrc: "https://source.unsplash.com/random/24",
+    },
+    {
+      imgSrc: "https://source.unsplash.com/random/25",
+    },
+    {
+      imgSrc: "https://source.unsplash.com/random/26",
+    },
+    {
+      imgSrc: "https://source.unsplash.com/random/27",
+    },
+    {
+      imgSrc: "https://source.unsplash.com/random/28",
+    },
+    {
+      imgSrc: "https://source.unsplash.com/random/29",
+    },
+    {
+      imgSrc: "https://source.unsplash.com/random/30",
+    },
+  ]);
+
+  const handleChange = ({ target }) => {
+    let value = target.value;
+    switch (target.name) {
+      case "columns":
+        if (value < 1) value = 1;
+        setColumns(parseInt(value) || 4);
+        break;
+      case "gap":
+        if (value < 0) value = 0;
+        setGap(parseInt(value) || 0);
+        break;
+      case "minWidth":
+        if (value < 1) value = 1;
+        setMinWidth(parseInt(value) || 200);
+        break;
+      case "color":
+        setBackgroundColor(value);
+        break;
+      default:
+        console.log("Invalid target");
+    }
+  };
+
   return (
     <div className="App">
-      <Bricks />
+      <div className="container">
+        <div className="col form">
+          <p>
+            <label htmlFor="columns">Columns</label>
+            <input
+              type="number"
+              name="columns"
+              value={columns}
+              onChange={handleChange}
+            />
+            <span>Default: 4 Minimum: 1</span>
+          </p>
+          <p>
+            <label htmlFor="gap">Gap</label>
+            <input
+              type="number"
+              name="gap"
+              value={gap}
+              onChange={handleChange}
+            />
+            <span>Default: 0 Minimum: 0</span>
+          </p>
+          <p>
+            <label htmlFor="minWidth">Min Width</label>
+            <input
+              type="number"
+              name="minWidth"
+              value={minWidth}
+              onChange={handleChange}
+            />
+            <span>Default: 200 Minimum: 1</span>
+          </p>
+          <p>
+            <label htmlFor="color">Background Color</label>
+            <input
+              type="text"
+              name="color"
+              value={backgroundColor}
+              onChange={handleChange}
+            />
+            <span>
+              Supports named colors (e.g. red, green, blue etc) or hex value
+              (e.g. #ffffff)
+            </span>
+          </p>
+        </div>
+        <div className="col">
+          <p>The below code snippet shows the component in use</p>
+          <p>{`<Bricks data={data} columns={${columns}} gap={${gap}} minWidth={${minWidth}} backgroundColor={${backgroundColor}} />`}</p>
+          <p>
+            Photos from{" "}
+            <a href="https://source.unsplash.com/" target="blank">
+              Unsplash Source
+            </a>
+          </p>
+        </div>
+      </div>
+      <Bricks
+        data={data}
+        columns={columns}
+        gap={gap}
+        minWidth={minWidth}
+        backgroundColor={backgroundColor}
+      />
     </div>
   );
 }
