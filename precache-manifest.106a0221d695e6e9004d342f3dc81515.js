33cc9023b1b2b9961a81754cf7d4df7f64c1faf3
Mike <ballardm123@gmail.com>
2020-04-07 12:00:25 +0100

    Updates

---
+++ precache-manifest.106a0221d695e6e9004d342f3dc81515.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d240d9f25e6523c66a55535d0c54e35b",
+    "url": "/react-responsive-bricks/index.html"
+  },
+  {
+    "revision": "23fec85ddeb3576d57c7",
+    "url": "/react-responsive-bricks/static/css/main.ae81ae26.chunk.css"
+  },
+  {
+    "revision": "e0ff2295031270d33f05",
+    "url": "/react-responsive-bricks/static/js/2.5e25bfb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-responsive-bricks/static/js/2.5e25bfb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23fec85ddeb3576d57c7",
+    "url": "/react-responsive-bricks/static/js/main.02f5cbb5.chunk.js"
+  },
+  {
+    "revision": "4857c53f3dbf0738a19f",
+    "url": "/react-responsive-bricks/static/js/runtime-main.c9c67c10.js"
+  }
+]);
